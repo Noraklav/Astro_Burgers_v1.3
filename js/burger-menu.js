a01b1ab4156bf80f5083779161d8df42828af90b
Agustin <agustin.dv.2021@gmail.com>
2021-12-26 00:09:03 -0300

    SCSS Prolijo

--- js/burger-menu.js
+++
@@ -1,9 +0,0 @@
-const iconoMenu = document.querySelector("#icono-menu");
-const menu = document.querySelector("#menu");
-
-iconoMenu.addEventListener('click', (e) =>{
-    // toggle: Mostrar un div mientras se oculta otro
-    // Alternar estilos para el menu y el body
-    menu.classList.toggle('active');
-    document.body.toggle('opacity');
-})
